f4976ea1522db9ab032e17f5868d45720f310950
centraltee <tmgdaman14@gmail.com>
2025-05-03 21:46:50 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Tadiwa', // Your name
+  botOwner: 'Tadiwag', // Your name
   ownerNumber: '263789304671', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
